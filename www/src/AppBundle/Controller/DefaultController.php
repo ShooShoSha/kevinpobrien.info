b3130acf87d700adaac6199b8ea847e697908e90
Kevin O'Brien <shooshosha@gmail.com>
2016-06-06 14:12:37 -0700

    Chage template render path.

--- www/src/AppBundle/Controller/DefaultController.php
+++ www/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,8 +15,16 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('default/home.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/resume", name="resume")
+     */
+    public function resumeAction(Request $request)
+    {
+        return $this->render('default/resume.html.twig', array());
     }
 }
